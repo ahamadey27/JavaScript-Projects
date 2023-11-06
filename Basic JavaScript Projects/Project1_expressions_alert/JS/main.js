e496053a3360d6ee4433270f3f89ee169b91e4e5
ahamadey27 <hamadey@gmail.com>
2023-11-06 15:44:20 -0500

    Project_1 Done

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var B = "Window Variable"
+window.alert(B);  
+document.write("Writting with java!"); 
+var A = "My First String Variable."
+document.write(A); 
+var C = "\"please don't hurt 'em hammer!\""
+document.write(C); 
+var D = "\"I\'m trying to type something,"
+    + " something that makes some sense!"
+document.write(D); 
+
+var starter = "oranges", main = "chicken", dessert = "pie"; 
+document.write(main); //this does lots of wonderful things! 
