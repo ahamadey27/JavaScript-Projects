bb87e9beb823076df7cadacfa7fc6320bd7a7e69
ahamadey27 <hamadey@gmail.com>
2023-11-06 17:59:35 -0500

    Java Project 2 Finished

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function Project_2_Function() {
+    var button1 = "BT1";
+    var button2 = "BT2";
+    var str = "Here a paragraph for this exercise"; 
+    str += " and some more words"; 
+    document.getElementById("paragraph").innerHTML = str; 
+}
+
