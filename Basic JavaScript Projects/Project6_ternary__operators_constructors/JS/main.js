10f505739fa7be483428aec1302886380e1cf3bf
ahamadey27 <hamadey@gmail.com>
2023-11-07 15:51:58 -0500

    RESERVED KEYWORD CHALLENGE

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,6 +1,6 @@
-function Vote_Function() {
-    var Age, Can_vote;
-    Age = document.getElementById("Age").value; 
-    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
-    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+function Compressor(type, maker, model) {
+    this.compressorType = type;
+    this.companyName = maker;
+    this.modelName = model;
+    document.getElementById("comp").innerHTML = companyName; 
 }
