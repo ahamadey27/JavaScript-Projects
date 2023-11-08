03f488f868178a4dc1a8ad1c3572520bb3d89550
ahamadey27 <hamadey@gmail.com>
2023-11-08 12:58:19 -0500

    Search Method

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,5 +1,5 @@
-function to_Upper_Case() {
-    const str = "we are going to become upper case" 
-    const toUpper = str.toUpperCase(); 
-    document.getElementById("Upper").innerHTML = toUpper; 
+function search_Method() {
+    const str = "hello this is going to be a long sentance and a good time" 
+    const toSearch = str.search("time"); 
+    document.getElementById("Search").innerHTML = toSearch; 
 }
