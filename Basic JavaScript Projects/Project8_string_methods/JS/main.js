79f5520abdca711a5d8d85a4cd7373948acfac68
ahamadey27 <hamadey@gmail.com>
2023-11-08 12:47:45 -0500

    SLICE METHOD

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,8 +1,5 @@
-function Full_Sentance(){
-    var S1 = "I'm going to the store ";
-    var S2 = "to but some eggs for a cake. ";
-    var S3 = "I really hope I don't mess this one up! ";
-    var S4 = "People are counting on me.";
-    var Complete_Sentance = S1.concat(S2, S3, S4); 
-    document.getElementById("Sentance_Time").innerHTML = Complete_Sentance;
+function slice_Method() {
+    var Sentence1 = "1234567890123456789012345678901";
+    var Section1 = Sentence1.slice(12,16);
+    document.getElementById("Slice").innerHTML = Section1; 
 }
