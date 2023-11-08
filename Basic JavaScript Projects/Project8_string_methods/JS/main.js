2aef73f87701859cad1906ac10020b3ebef43e1b
ahamadey27 <hamadey@gmail.com>
2023-11-08 14:56:22 -0500

    TOPRECISION() METHOD ASSIGNMENT

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
 function number_Strings() {
-    var Y = 1432;
-    document.getElementById("numbers_to_string").innerHTML = Y.toString(); 
+    var Y = 243423425
+    document.getElementById("numbers_to_string").innerHTML = Y.toPrecision(10);  
 }
