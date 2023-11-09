9a7b729d005d94f5c9e1ed3028ec9e1ef85b18cf
ahamadey27 <hamadey@gmail.com>
2023-11-09 16:18:03 -0500

    Arrays

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,11 +1,12 @@
-var Instruments = ["gutiar", "drums", "piano", "bass", "flute"]; 
-var Content = ""; 
-var Y;
-function for_Loop() {
-    for (Y = 0; Y < Instruments.length; Y++){
-        Content += Instruments[Y] + "<br>";
-    }
-    document.getElementById("List_of_Instruments").innerHTML = Content;
+function array_Function() {
+    var Game_State = [];
+    Game_State[0] = "still";
+    Game_State[1] = "walking";
+    Game_State[2] = "fast walking"; 
+    Game_State[3] = "running"; 
+    Game_State[4] = "dead";
+    document.getElementById("Array").innerHTML = "In the end of the game, the character is " + Game_State[4] + "."; 
+
 }
 
-
+//Index is array number and value is what the array equals
