1d715230079c709b5a2927d4616c486e80d6878b
ahamadey27 <hamadey@gmail.com>
2023-11-09 16:56:41 -0500

    LET KEYWORD ASSIGNMENT

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,7 +1,7 @@
-function constant_Function() {
-    const computer = { type: "Desktop", brand: "Mac", color: "gray" }; 
-    computer.price = "1000"; 
-    computer.color = "pink"; 
-    document.getElementById("Constant").innerHTML = "The color of the " +
-        computer.type + " is " + computer.color + "."; 
+function let_Function() {
+    var x = 22;
+    {
+        let x = 33;
+    }
+    document.getElementById("Constant").innerHTML = "<br>" + x; 
 }
