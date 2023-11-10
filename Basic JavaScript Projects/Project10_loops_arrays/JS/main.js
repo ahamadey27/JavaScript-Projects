77f5c950f7a852ef71ac3071c6ce89a9535c3da8
ahamadey27 <hamadey@gmail.com>
2023-11-10 10:27:44 -0500

    Break Statement

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,6 +1,6 @@
 let text = "";
 for (let i = 0; i < 10; i++) {
-    if (i === 3) { continue; }
+    if (i === 3) { break; }
     text += "The number is " + i + "<br>";
 }
 document.getElementById("continueDemo").innerHTML = text;
