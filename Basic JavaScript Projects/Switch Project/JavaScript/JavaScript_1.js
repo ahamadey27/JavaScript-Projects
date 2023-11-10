325288ec83640b8ed75c75aa27b7d83330f33c3a
ahamadey27 <hamadey@gmail.com>
2023-11-10 12:17:15 -0500

    DOCUMENT.GETELEMENTSBYCLASSNAME() METHOD ASSIGNMENT

--- Basic JavaScript Projects/Switch Project/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/Switch Project/JavaScript/JavaScript_1.js
@@ -1,25 +1,4 @@
-function Animal_Function() {
-    var Animal_Output;
-    var Animals = document.getElementById("Animal_Input").value; 
-    var Animal_String = " is a great animal!"; 
-    switch (Animals) {
-        case "Turtle":
-            Animal_Output = "Turtle" + Animal_String;
-            break;
-        case "Cat":
-            Animal_Output = "Cat" + Animal_String;
-            break;
-        case "Dog":
-            Animal_Output = "Dog" + Animal_String;
-            break;
-        case "Fish":
-            Animal_Output = "Fish" + Animal_String;
-            break;
-        case "Snake":
-            Animal_Output = "Snake" + Animal_String;
-            break;
-        default:
-            Animal_Output = "Please enter animal exactly as written in the list.";
-    }
-    document.getElementById("Output").innerHTML = Animal_Output; 
+function elementByClass() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "This Text Will Change"
 }
