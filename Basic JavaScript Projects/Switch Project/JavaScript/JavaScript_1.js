438853110c23b1e8a9a81c3c625dd51182d14237
ahamadey27 <hamadey@gmail.com>
2023-11-10 12:32:05 -0500

    basic contact form

--- Basic JavaScript Projects/Switch Project/JavaScript/JavaScript_1.js
+++ Basic JavaScript Projects/Switch Project/JavaScript/JavaScript_1.js
@@ -1,4 +1,11 @@
-function elementByClass() {
-    var A = document.getElementsByClassName("Click");
-    A[0].innerHTML = "This Text Will Change"
-}
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+// Create a Gradient
+const grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+// Draw a filled Rectangle
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
